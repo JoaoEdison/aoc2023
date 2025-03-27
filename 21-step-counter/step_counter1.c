66bd74754d60a75cf8078c22fa978b30133ec33f
JoaoEdison <joaoedisonmanica@gmail.com>
2025-03-27 20:21:47 -0300

    First Part, Twenty-First Day.

---
+++ 21-step-counter/step_counter1.c
@@ -0,0 +1,153 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../base_structures/linkedlist.c"
+#include "../base_structures/avltree.c"
+
+#define MAX_BLOCKS 3000
+#define DISTANCE 64
+#define TO_1D(X,Y)\
+    ((X) * max_d) + Y
+
+int x,y;
+int max_d;
+
+void read_data(blocks, startx, starty)
+avl_tree *blocks;
+int *startx, *starty;
+{
+    size_t size;
+    char *line;
+    int *new_block;
+    int new_blocks[MAX_BLOCKS][2];
+    int new_blocks_end = 0;
+    
+    y = size = 0;
+    line = NULL;
+    while (getline(&line, &size, stdin) > 2) {
+        for (x=0; line[x] != '\n'; x++)
+            if (line[x] == '#') {
+                new_blocks[new_blocks_end  ][0] = x;
+                new_blocks[new_blocks_end++][1] = y;
+                assert(new_blocks_end < MAX_BLOCKS);
+            } else if (line[x] == 'S') {
+                *startx = x;
+                *starty = y;
+            }
+        y++;
+    }
+    free(line);
+    max_d = x > y? x : y;
+    while (new_blocks_end--) {
+        new_block = malloc(sizeof(int));
+        *new_block = TO_1D(new_blocks[new_blocks_end][0], new_blocks[new_blocks_end][1]);
+        insert_avltree(blocks, new_block, NULL);
+    }
+}
+
+typedef struct {
+    int i,j,steps;
+} path;
+
+comp_coordinate(c1, c2)
+void *c1, *c2;
+{
+    return *(int*)c1 - *(int*)c2;
+}
+
+traverse(blocks, startx, starty)
+avl_tree *blocks;
+{
+    path *p, curr, *new_path;
+    list_linkedlist q = { 0 };
+    avl_tree visited;
+    int coord_1d, *new_location, counter;
+       
+    create_alvtree(visited, comp_coordinate);
+
+    curr.i = startx;
+    curr.j = starty;
+    new_location = malloc(sizeof(int));
+    *new_location = TO_1D(curr.i, curr.j);
+    insert_avltree(&visited, new_location, NULL);
+
+    counter = curr.steps = 0;
+    new_path = malloc(sizeof(path));
+    memcpy(new_path, &curr, sizeof(path));
+    append_linkedlist(&q, new_path);
+    while ((p = pop_linkedlist(&q))) {
+        coord_1d = TO_1D(p->i, p->j);
+        if (p->steps > DISTANCE) {
+            free(p);
+            continue;
+        }
+        curr.steps = p->steps + 1;
+        if ((p->i + p->j) % 2 == DISTANCE % 2)
+            counter++;            
+        if (p->j < y-1) {
+            curr.i = p->i;
+            curr.j = p->j+1;
+            coord_1d = TO_1D(curr.i, curr.j);
+            if (!(find_node_avltree_ptr(blocks, &coord_1d) || find_node_avltree(visited, &coord_1d))) {
+                new_location = malloc(sizeof(int));
+                *new_location = coord_1d;
+                insert_avltree(&visited, new_location, NULL);
+                new_path = malloc(sizeof(path));
+                memcpy(new_path, &curr, sizeof(path));
+                append_linkedlist(&q, new_path);
+            }
+        }
+        if (p->i < x-1) {
+            curr.i = p->i+1;
+            curr.j = p->j;
+            coord_1d = TO_1D(curr.i, curr.j);
+            if (!(find_node_avltree_ptr(blocks, &coord_1d) || find_node_avltree(visited, &coord_1d))) {
+                new_location = malloc(sizeof(int));
+                *new_location = coord_1d;
+                insert_avltree(&visited, new_location, NULL);
+                new_path = malloc(sizeof(path));
+                memcpy(new_path, &curr, sizeof(path));
+                append_linkedlist(&q, new_path);
+            }
+        }
+        if (p->i) {
+            curr.i = p->i-1;
+            curr.j = p->j;
+            coord_1d = TO_1D(curr.i, curr.j);
+            if (!(find_node_avltree_ptr(blocks, &coord_1d) || find_node_avltree(visited, &coord_1d))) {
+                new_location = malloc(sizeof(int));
+                *new_location = coord_1d;
+                insert_avltree(&visited, new_location, NULL);
+                new_path = malloc(sizeof(path));
+                memcpy(new_path, &curr, sizeof(path));
+                append_linkedlist(&q, new_path);
+            }
+        }
+        if (p->j) {
+            curr.i = p->i;
+            curr.j = p->j-1;
+            coord_1d = TO_1D(curr.i, curr.j);
+            if (!(find_node_avltree_ptr(blocks, &coord_1d) || find_node_avltree(visited, &coord_1d))) {
+                new_location = malloc(sizeof(int));
+                *new_location = coord_1d;
+                insert_avltree(&visited, new_location, NULL);
+                new_path = malloc(sizeof(path));
+                memcpy(new_path, &curr, sizeof(path));
+                append_linkedlist(&q, new_path);
+            }
+        }
+        free(p);
+    }
+    destroy_avltree(visited);
+    return counter;
+}
+
+main()
+{
+    avl_tree blocks;
+    int startx, starty;
+
+    create_alvtree(blocks, comp_coordinate);
+    read_data(&blocks, &startx, &starty);
+    printf("%d\n", traverse(&blocks, startx, starty));
+    destroy_avltree(blocks);
+}
--- base_structures/linkedlist.c
+++ base_structures/linkedlist.c
@@ -1,36 +1,53 @@
-/* Código escrito por João Edison Roso Manica */
+/*
+    Doubly linked list implementation in C.
+    Copyright (C) 2025  João Manica
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 typedef struct node {
-	void *value;
-	struct node *next, *prev;
+    void *value;
+    struct node *next, *prev;
 } node_linkedlist;
 
 typedef struct {
-	node_linkedlist *head, *tail;
-	int nmemb;
+    node_linkedlist *head, *tail;
+    int nmemb;
 } list_linkedlist;
 
 void append_linkedlist(l, item)
 list_linkedlist *l;
 void *item;
 {
-	node_linkedlist *new;
-	
-	new = malloc(sizeof(node_linkedlist));
-	new->value = item;
-	new->next = NULL;
-	if (l->head) {
-		l->tail->next = new;
-		new->prev = l->tail;
-	} else {
-		l->head = new;
-		new->prev = NULL;
-	}
-	l->tail = new;
-	l->nmemb++;
+    node_linkedlist *new;
+    
+    new = malloc(sizeof(node_linkedlist));
+    new->value = item;
+    new->next = NULL;
+    if (l->head) {
+        l->tail->next = new;
+        new->prev = l->tail;
+    } else {
+        l->head = new;
+        new->prev = NULL;
+    }
+    l->tail = new;
+    l->nmemb++;
 }
 
 void appendlloc_linkedlist(l, item, size)
@@ -38,21 +55,21 @@ list_linkedlist *l;
 void *item;
 size_t size;
 {
-	node_linkedlist *new;
-	
-	new = malloc(sizeof(node_linkedlist));
-	new->value = malloc(size);
-	memcpy(new->value, item, size);
-	new->next = NULL;
-	if (l->head) {
-		l->tail->next = new;
-		new->prev = l->tail;
-	} else {
-		l->head = new;	
-		new->prev = NULL;
-	}
-	l->tail = new;
-	l->nmemb++;
+    node_linkedlist *new;
+    
+    new = malloc(sizeof(node_linkedlist));
+    new->value = malloc(size);
+    memcpy(new->value, item, size);
+    new->next = NULL;
+    if (l->head) {
+        l->tail->next = new;
+        new->prev = l->tail;
+    } else {
+        l->head = new;    
+        new->prev = NULL;
+    }
+    l->tail = new;
+    l->nmemb++;
 }
 
 void *find_linkedlist(l, mock, compar)
@@ -60,49 +77,50 @@ list_linkedlist *l;
 void *mock;
 int (*compar)(const void *, const void *);
 {
-	node_linkedlist *next;
-	
-	for (next=l->head;next;next=next->next)
-		if (!compar(next->value, mock))
-			return next->value;
-	return NULL;
+    node_linkedlist *next;
+    
+    for (next=l->head;next;next=next->next)
+        if (!compar(next->value, mock))
+            return next->value;
+    return NULL;
 }
 
 void *pop_linkedlist(l)
 list_linkedlist *l;
 {
-	node_linkedlist *h;
-	void *item;
-	
-	item = NULL;
-	if ((h = l->head)) {
-		item = h->value;
-		l->head = h->next;
-		l->head->prev = NULL;
-		l->nmemb--;
-		free(h);
-	}
-	return item;
+    node_linkedlist *h;
+    void *item;
+    
+    item = NULL;
+    if ((h = l->head)) {
+        item = h->value;
+        l->head = h->next;
+        if (h->next)
+            l->head->prev = NULL;
+        l->nmemb--;
+        free(h);
+    }
+    return item;
 }
 
 node_linkedlist *delete_item_linkedlist(l, item)
 list_linkedlist *l;
 node_linkedlist *item;
 {
-	node_linkedlist *next;
-	
-	next = item->next;	
-	if (item->prev)
-		item->prev->next = item->next;
-	else
-		l->head = item->next;
-	if (item->next)
-		item->next->prev = item->prev;
-	else
-		l->tail = item->prev;
-	l->nmemb--;
-	free(item);
-	return next;
+    node_linkedlist *next;
+    
+    next = item->next;    
+    if (item->prev)
+        item->prev->next = item->next;
+    else
+        l->head = item->next;
+    if (item->next)
+        item->next->prev = item->prev;
+    else
+        l->tail = item->prev;
+    l->nmemb--;
+    free(item);
+    return next;
 }
 
 /* CAUTION: Assumes that arr has sufficient capacity. */
@@ -111,10 +129,10 @@ unsigned char *arr;
 list_linkedlist *l;
 size_t size;
 {
-	node_linkedlist *next;
-	int i;
-	
-	for (i=0, next=l->head;next;next=next->next, i++)
-		memcpy(&arr[i*size], next->value, size);
-	return i;
+    node_linkedlist *next;
+    int i;
+    
+    for (i=0, next=l->head;next;next=next->next, i++)
+        memcpy(&arr[i*size], next->value, size);
+    return i;
 }
